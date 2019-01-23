60b2ed4e1fba42c129f995d58643de1011de9465
BLUE 2 <BlUE2@ratpack.com>
2019-01-22 20:10:45 -0500

    Updated space achievement

--- web/custom.js
+++ web/custom.js
@@ -5,9 +5,10 @@ $(function () {
         if (comment.indexOf("rainbow") != -1) {
             $('.content').css('background', 'linear-gradient(to bottom, rgba(255,44,40,1) 0%,rgba(255,128,0,1) 17%,rgba(255,246,0,1) 34%,rgba(0,170,0,1) 50%,rgba(0,219,201,1) 66%,rgba(43,0,216,1) 83%,rgba(165,46,121,1) 100%)');
         } else if (comment.indexOf("space")!= -1) {
-            $('.content').css('background', 'linear-gradient(to bottom, rgba(0,0,200,50) 0%, rgba(0,0,150,25) 33%, rgba(0,0,100,10) 66%, rgba(0,0,50,0) 100%)');
+            $('.content').css('background', 'linear-gradient(to bottom, rgba(0,0,0,50) 0%, rgba(0,25,50,75) 33%, rgba(0,50,100,100) 66%, rgba(0,75,120,200) 100%)');
         } else {
             $('.content').css('background', 'white');
+
         } 
         if (comment.indexOf("fire") != -1) {
             alert("Dexter, is that you?");
